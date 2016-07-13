hansec/autocomplete-fortran

commit c7aebaccf3703c49bc2f17af2e3df954606a5f60

Preserve case in completion suggestions

python/parse_fortran.py
@@ -179,7 +179,7 @@ def read_fun_def(line, return_type=None):
     if paren_match is not None:
         word_match = WORD_REGEX.findall(paren_match.group(0))
         if word_match is not None:
-            word_match = [word.lower() for word in word_match]
+            word_match = [word for word in word_match]
             args = ','.join(word_match)
         trailing_line = trailing_line[paren_match.end(0):]
     #
@@ -210,7 +210,7 @@ def read_sub_def(line):
     if paren_match is not None:
         word_match = WORD_REGEX.findall(paren_match.group(0))
         if word_match is not None:
-            word_match = [word.lower() for word in word_match]
+            word_match = [word for word in word_match]
             args = ','.join(word_match)
         trailing_line = trailing_line[paren_match.end(0):]
     return 'sub', [name, args]
@@ -308,16 +308,16 @@ class fortran_scope:
     def __init__(self, line_number, name, enc_scope=None, args=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.parent = None
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
     def set_visibility(self, new_vis):
         self.vis = new_vis
     def add_use(self, use_mod, only_list=[]):
@@ -371,16 +371,16 @@ class fortran_module(fortran_scope):
     def __init__(self, line_number, name, enc_scope=None, args=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.parent = None
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
     def get_type(self):
         return 1
     def get_desc(self):
@@ -390,16 +390,16 @@ class fortran_program(fortran_scope):
     def __init__(self, line_number, name, enc_scope=None, args=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.parent = None
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
     def get_type(self):
         return 1
     def get_desc(self):
@@ -409,16 +409,16 @@ class fortran_subroutine(fortran_scope):
     def __init__(self, line_number, name, enc_scope=None, args=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.parent = None
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
     def get_type(self):
         return 2
     def get_desc(self):
@@ -428,7 +428,7 @@ class fortran_function(fortran_scope):
     def __init__(self, line_number, name, enc_scope=None, args=None, return_type=None, result_var=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.result_var = result_var
@@ -442,9 +442,9 @@ class fortran_function(fortran_scope):
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
     def get_type(self):
         return 3
     def get_desc(self):
@@ -462,7 +462,7 @@ class fortran_type(fortran_scope):
     def __init__(self, line_number, name, modifiers, enc_scope=None, args=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.modifiers = modifiers
@@ -470,9 +470,9 @@ class fortran_type(fortran_scope):
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
         for modifier in self.modifiers:
             if modifier == 4:
                 self.vis = 1
@@ -487,16 +487,16 @@ class fortran_int(fortran_scope):
     def __init__(self, line_number, name, enc_scope=None, args=None):
         self.sline = line_number
         self.eline = None
-        self.name = name.lower()
+        self.name = name
         self.children = []
         self.use = []
         self.parent = None
         self.vis = 0
         self.args = args
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
     def add_child(self, child_fqn):
         self.children.append(child_fqn)
     def get_type(self):
@@ -517,7 +517,7 @@ class fortran_int(fortran_scope):
 class fortran_obj:
     def __init__(self, line_number, name, var_desc, modifiers, enc_scope=None, link_obj=None):
         self.sline = line_number
-        self.name = name.lower()
+        self.name = name
         self.desc = var_desc
         self.modifiers = modifiers
         self.children = []
@@ -527,9 +527,9 @@ class fortran_obj:
         else:
             self.link_obj = None
         if enc_scope is not None:
-            self.FQSN = enc_scope.lower() + "::" + self.name
+            self.FQSN = enc_scope.lower() + "::" + self.name.lower()
         else:
-            self.FQSN = self.name
+            self.FQSN = self.name.lower()
         for modifier in self.modifiers:
             if modifier == 4:
                 self.vis = 1

lib/autocomplete-fortran.coffee
@@ -13,7 +13,13 @@ module.exports =
       default: 2
       order: 2
       title: 'Minimum word length'
-      description: "Only autocomplete when you have typed at least this many characters. Note: autocomplete is always active for user-defined type fields"
+      description: "Only autocomplete when you have typed at least this many characters. Note: autocomplete is always active for user-defined type fields."
+    preserveCase:
+      type: 'boolean'
+      default: true
+      order: 3
+      title: 'Preserve completion case'
+      description: "Preserve case of suggestions from their defintion when inserting completions. Otherwise all suggestions will be lowercase."
   provider: null
 
   activate: ->

lib/fortran-provider.coffee
@@ -15,6 +15,7 @@ class FortranProvider
   pythonPath: ''
   parserPath: ''
   minPrefix: 2
+  preserveCase: true
   firstRun: true
   indexReady: false
   lastFile: ''
@@ -33,6 +34,7 @@ class FortranProvider
     @pythonPath = atom.config.get('autocomplete-fortran.pythonPath')
     @parserPath = path.join(__dirname, "..", "python", "parse_fortran.py")
     @minPrefix = atom.config.get('autocomplete-fortran.minPrefix')
+    @preserveCase = atom.config.get('autocomplete-fortran.preserveCase')
     @saveWatchers = new CompositeDisposable
     @workspaceWatcher = atom.workspace.observeTextEditors((editor) => @setupEditors(editor))
 
@@ -286,7 +288,7 @@ class FortranProvider
         return @buildCompletionList(suggestions)
       if prefix.length < @minPrefix and not activatedManually
         return completions
-      for key in @globalObjInd when (@projectObjList[key]['name'].startsWith(prefixLower))
+      for key in @globalObjInd when (@projectObjList[key]['name'].toLowerCase().startsWith(prefixLower))
         if @projectObjList[key]['type'] == 1
           continue
         suggestions.push(key)
@@ -363,7 +365,7 @@ class FortranProvider
       matches = line.match(wordRegex)
       if matches.length == 2
         if prefixLower?
-          for key in @globalObjInd when (@projectObjList[key]['name'].startsWith(prefixLower))
+          for key in @globalObjInd when (@projectObjList[key]['name'].toLowerCase().startsWith(prefixLower))
             if @projectObjList[key]['type'] != 1
               continue
             suggestions.push(key)
@@ -525,13 +527,14 @@ class FortranProvider
     unless children?
       return
     for child in children
+      childLower = child.toLowerCase()
       if prefix?
-        unless child.startsWith(prefix)
+        unless childLower.startsWith(prefix)
           continue
       if onlyList.length > 0
-        if onlyList.indexOf(child) == -1
+        if onlyList.indexOf(childLower) == -1
           continue
-      childKey = scope+'::'+child
+      childKey = scope+'::'+childLower
       if childKey of @projectObjList
         completions.push(childKey)
     # Add inherited
@@ -590,13 +593,14 @@ class FortranProvider
     if 'vis' of scopeObj
       currVis = parseInt(scopeObj['vis'])
     for child in children
+      childLower = child.toLowerCase()
       if prefix?
-        unless child.startsWith(prefix)
+        unless childLower.startsWith(prefix)
           continue
       if onlyList.length > 0
-        if onlyList.indexOf(child) == -1
+        if onlyList.indexOf(childLower) == -1
           continue
-      childKey = scope+'::'+child
+      childKey = scope+'::'+childLower
       childObj = @projectObjList[childKey]
       if childObj?
         if 'vis' of childObj
@@ -740,6 +744,8 @@ class FortranProvider
     compObj = {}
     compObj.type = @mapType(suggestion['type'])
     compObj.leftLabel = @descList[suggestion['desc']]
+    unless @preserveCase
+      name = name.toLowerCase()
     if 'args' of suggestion
       argStr = suggestion['args']
       if stripArg
@@ -748,6 +754,8 @@ class FortranProvider
           argStr = argStr.substring(i1+1).trim()
         else
           argStr = ''
+      unless @preserveCase
+        argStr = argStr.toLowerCase()
       compObj.snippet = name + "(" + argStr + ")"
     else
       compObj.text = name

commit a134d61a2dcae78825abe89b0fd4d31a13c6e329

Fix bug with user-defined type fields introduced in c7aebaccf3703c49bc2f17af2e3df954606a5f60
 - Uppercase user-defined type fields caused suggestion object to not be found when building completion list

python/parse_fortran.py
@@ -354,7 +354,7 @@ class fortran_scope:
             scope_dict['args'] = arg_str
         if len(self.children) > 0:
             for child in self.children:
-                scope_dict['mem'].append(child.name)
+                scope_dict['mem'].append(child.name.lower())
         if len(self.use) > 0:
             scope_dict['use'] = []
             for use_stmt in self.use:
@@ -506,7 +506,7 @@ class fortran_int(fortran_scope):
     def write_scope(self):
         child_list = []
         for child in self.children:
-            child_list.append(child)
+            child_list.append(child.lower())
         scope_dict = {'name': self.name, 'type': 7, 'fbound': [self.sline, self.eline], 'mem': child_list}
         if self.vis == -1:
             scope_dict['vis'] = '-1'